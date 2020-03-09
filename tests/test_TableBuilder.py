905b196af5aa92bbe4bac721a70297e496aedcb1
James Dean <jamesdean268@gmail.com>
2020-03-09 19:34:25 +1100

    Testing suite updated and main script added

--- tests/test_TableBuilder.py
+++ tests/test_TableBuilder.py
@@ -8,24 +8,37 @@ from src.HTMLScraper import HTMLScraper
 
 class TableBuilder_Test(unittest.TestCase):
 
-    # Test to check if all years and all clubs return >= 22 tables = pctTable
-    # Pct table is used in the main script to determine which games were played
-    def test_scrapeWebAndParseHTML_WebScraped_22TablesReturned(self):
+    # Spot test of player Stats
+    def test_getPlayerStats_TableCreated_ValuesExpected(self):
         # Arrange
         aflHTMLScraper = HTMLScraper()
         tableBuilder = TableBuilder(aflHTMLScraper)
-        playerStats = tableBuilder.getPlayerStats()
-        gamesPlayed = tableBuilder.getGamesPlayed()
         # Act
+        playerStats = tableBuilder.getPlayerStats()
+        # Assert
         p = 0
         print(playerStats[p][0]) # Brown, Luke
+        self.assertEqual(playerStats[p][0], 'Brown, Luke')
         print(playerStats[p][1]) # 2012
+        self.assertEqual(playerStats[p][1], 2012)
         print(playerStats[p][2]) # adelaide
+        self.assertEqual(playerStats[p][2], 'adelaide')
         print(playerStats[p][3]) # 3
+        self.assertEqual(playerStats[p][3], 3)
         print(playerStats[p][4]) # 19
+        self.assertEqual(playerStats[p][4], '19')
         print(playerStats[p][11]) # 2
-        print(gamesPlayed[12][:]) # Callinan, Ian, 2012, adelaide, R12
-        # Assert
+        self.assertEqual(playerStats[p][11], '2')
 
-        self.assertEqual(1,1)
+    # Spot test of player Stats
+    def test_getGamesPlayed_TableCreated_ValuesExpected(self):
+        # Arrange
+        aflHTMLScraper = HTMLScraper()
+        tableBuilder = TableBuilder(aflHTMLScraper)
+        # Act
+        gamesPlayed = tableBuilder.getGamesPlayed()
+        # Assert
+        print(gamesPlayed[12][3]) # R12
+        self.assertEqual(gamesPlayed[12][3], 'R12')
+
 
