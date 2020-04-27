b9769683bf27b490e9f52bd89461e038dfcd4645
James Dean <jamesdean268@gmail.com>
2020-04-27 18:17:27 +1000

    Started to build query

--- Main.py
+++ Main.py
@@ -1,2 +1,25 @@
 
+# Imports
+import os
+from src.Sqlite3Database import Sqlite3Database
+
 # Main exeuction script
+
+# Connect to SQLite3 database
+pwd = os.getcwd()
+pathToDatabase = pwd + '/data/AFLFootyTips.db'
+aflSqlite3Database = Sqlite3Database(pathToDatabase)
+
+# Build query
+query_GamesPlayedWithStats =  ( "SELECT * FROM GAMES_PLAYED "
+                                "OUTER JOIN PLAYER_STATS "
+                                "ON " 
+                                "GAMES_PLAYED.player = PLAYER_STATS.player,"
+                                "GAMES_PLAYED.year = PLAYER_STATS.year,"
+                                "GAMES_PLAYED.team = PLAYER_STATS.team;"
+)
+
+print(aflSqlite3Database.runSqlite3Query(query_GamesPlayedWithStats))
+
+
+
---
+++ src/DatabaseQueries.py
@@ -0,0 +1,20 @@
+import os
+
+class DatabaseQueries:
+
+    # Dependency injection of HTML scraper and variables
+    def __init__(self, Sqlite3Database):
+        self._Sqlite3Database = Sqlite3Database
+
+
+    def consolidatePlayerStats(self):
+        print(1)
+
+
+
+
+
+
+# Execution of the class model for debugging purposes
+if __name__ == '__main__':
+	print(1)
